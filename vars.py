b57bae3520bfe0cb040f4971361176dbfdfb0793
sean-6 <60482524+sean-6@users.noreply.github.com>
2021-03-04 15:09:54 +0000

    Delete vars.py

--- vars.py
+++
@@ -1,6 +0,0 @@
-data = {
-    "BOT_USERNAME": "twitchplays_connect4",
-    "OAUTH_TOKEN": "oauth:dsiwvxxny2bpt555cy805rrfsmr5et",
-    "CHANNEL_NAME":"twitchplays_connect4",
-    "CLIENT_ID":"aceybfltidwfd8y4biobfycgjuef0v"
-}
