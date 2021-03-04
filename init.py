0020c902b72162b83bda10c4e915d320dd3b5ff6
Sean Cahill <seanc23lfc@gmail.com>
2021-03-04 14:02:38 +0000

    adding

---
+++ init.py
@@ -0,0 +1,18 @@
+import string
+from vars import data
+
+def join(server):
+    readbuffer = ""
+    Loading = True
+    while Loading:
+        readbuffer = readbuffer + server.recv(2048).decode('utf-8')
+        temp = readbuffer.split("\n")
+        readbuffer = temp.pop()
+
+        for line in temp:
+            print(line)
+            Loading = complete(line)
+
+
+def complete:
+    
